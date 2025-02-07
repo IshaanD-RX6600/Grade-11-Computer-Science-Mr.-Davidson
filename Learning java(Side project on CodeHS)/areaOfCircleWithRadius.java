e02145ac380565997506e6624859094ff2cae1fe
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-06 22:43:57 -0500

    Add Java programs for calculating area of a circle and car carrier weight

---
+++ Learning java(Side project on CodeHS)/areaOfCircleWithRadius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class areaOfCircleWithRadius {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        double area = (int) (Math.PI * Math.pow(radius, 2));
+        System.out.println(area);
+    }
+}
---
+++ Learning java(Side project on CodeHS)/getCarCarrierWeight.java
@@ -0,0 +1,5 @@
+public class getCarCarrierWeight {
+    public static double getCarCarrierWeight(double carWeight, int numCars) {
+        return carWeight * numCars;
+    }
+}
