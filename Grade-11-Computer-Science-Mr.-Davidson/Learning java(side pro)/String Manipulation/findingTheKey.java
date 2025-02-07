8955bc3e5905b55d63ea812eaf2c8b24eec8ac0f
Ishaan Dhiman <dhimanishaan7@gmail.com>
2025-02-07 10:06:30 -0500

    Create findingTheKey.java

---
+++ Grade-11-Computer-Science-Mr.-Davidson/Learning java(side pro)/String Manipulation/findingTheKey.java
@@ -0,0 +1,11 @@
+public static findingTheKey{
+  public boolean phraseContainsKey(String phrase, String key) {
+    return phrase.contains(key);
+    /*
+    The method takes 2 parameters, phrase and key. 
+    It uses the contains.(); method to check whether or not the key is there or not
+    If the key is found within the phrase, it returns true.
+    If the key is not found it will return false
+    */
+  }
+}
