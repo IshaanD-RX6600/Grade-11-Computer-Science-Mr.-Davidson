2a37a160034064a04954ead70c8eb1c5dc4c5593
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-06 23:17:47 -0500

    Add lowerCaseCheck.java for string manipulation functionality

---
+++ Learning java(Side project on CodeHS)/String Manipulation/lowerCaseCheck.java
@@ -0,0 +1,13 @@
+public class lowerCaseCheck {
+    public boolean isLowerCase(String phrase)
+{
+    if(phrase.equals(phrase.toLowerCase()))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }   
+}
+}
