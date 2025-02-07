d9a2b16eee1feefd4055be5f0b83765308aba296
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-06 22:45:37 -0500

    Add purchaseCalculator class for summing four double values

---
+++ Learning java(Side project on CodeHS)/purchaseCalculator.java
@@ -0,0 +1,7 @@
+public class purchaseCalculator {
+    public static double Calculator(double first, double second, double third, double fourth)
+{
+   return first + second + third + fourth;
+}
+   
+}
