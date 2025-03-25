848553f41e9c5a4d14c4c6cc51719739c6e7fccd
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-03-25 16:30:36 -0400

    made some changes

---
+++ Python/codetalks2/negativeindices.py
@@ -0,0 +1,17 @@
+#negativeindices.py
+#I. Dhiman
+my_list = [10, 20, 30, 40, 50]
+
+print("Using positive indices:")
+print("my_list[1]:", my_list[1])  
+
+print("\nUsing negative indices:")
+print("my_list[-1]:", my_list[-1])  
+
+
+print("\nSlicing with negative indices:")
+print("my_list[-3:]:", my_list[-3:]) 
+print("my_list[:-2]:", my_list[:-2]) 
+
+print("\nExplanation:")
+print("Negative indices count from the end of the list, where -1 is the last element, -2 is the second last, and so on.")
